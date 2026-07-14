9d6298b58d7690925b3fb2fd9a56a329dc183aec
akula sathvik <akulasathvik7.com>
2026-07-14 12:47:15 +0530

    changes are added

---
+++ properties/shape.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+#to check the shape of the array  
+
+#to check the shape of the array
+
+#np.shape()
+
+lis=[[1,2,3],[1,2,3]]
+
+np_shape=np.shape(lis)
+
+print(np_shape)
+
+lis=[[[1,2],[1,2],[1,3]],[[1,2],[1,2],[1,3]],[[1,2],[1,3],[1,4]]]
+
+np_shape=np.shape(lis)
+
+print(np_shape)
---
+++ properties/size.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+#to check the size of the array
+
+#np.size()=>return the number of values presents in the array
+
+
+
+lis=[[1,2,3],[1,2,3]]
+
+np_size=np.size(lis)
+
+print(np_size)
+
+lis=[[[1,2],[1,2],[1,3]],[[1,2],[1,2],[1,3]],[[1,2],[1,3],[1,4]]]
+
+np_size=np.size(lis)
+
+print(np_size)
